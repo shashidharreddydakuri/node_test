40dd1b61be39f0f405e1a115afac197be7f69cee
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2021-02-26 21:16:34 +0530

    basic pagination

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const users = [
+	{ id: 1, name: 'User 1' },
+	{ id: 2, name: 'User 2' },
+	{ id: 3, name: 'User 3' },
+	{ id: 4, name: 'User 4' },
+	{ id: 5, name: 'User 5' },
+	{ id: 6, name: 'User 6' },
+	{ id: 7, name: 'User 7' },
+	{ id: 8, name: 'User 8' },
+	{ id: 9, name: 'User 9' },
+	{ id: 10, name: 'User 10' },
+];
+
+app.get('/users', (req, res) => {
+    const page = req.query.page
+    res.json(users)
+})
+
+
+app.listen(3000);
